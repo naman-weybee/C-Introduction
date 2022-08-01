4d2e8f56e6c743cdcaa04949c037f37fce9a37fb
naman-weybee <naman@weybee.in>
2022-08-01 12:14:02 +0530

    my first console app in .net framework

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 10;
+            int b = 5;
+            int total = a + b;
+            Console.WriteLine("Total: {0} + {1} = {2}", a, b, total);
+            Console.ReadLine();
+        }
+    }
+}
